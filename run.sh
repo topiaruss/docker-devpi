618077d3c0a8d3e009deb559c9cbf674eb0a6161
Russ Ferriday <russ.ferriday@swga.com>
2017-11-21 15:40:12 +0000

    Add concourse files

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# concourse trigger file
+set -ex
+
+creds=~/.concourse_credentials.yaml
+
+fly_target=lite
+fly -t lite login -c http://${CONCOURSE_HOST}:8080
+fly -t ${fly_target} sp configure -c pipeline.yaml -p docker-devpi --load-vars-from ${creds} -n
+fly -t ${fly_target} unpause-pipeline --pipeline docker-devpi
+
+# if you want to automatically trigger and watch
+#fly -t ${fly_target} trigger-job -j docker-devpi/docker-devpi-publish
+#fly -t ${fly_target} watch -j docker-devpi/docker-devpi-publish
